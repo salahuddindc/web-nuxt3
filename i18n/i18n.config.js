842ad0237fead84dcda27fa3f21d9756f5108ef5
salahuddindc <135432861+salahuddindc@users.noreply.github.com>
2024-11-05 12:02:07 +0500

    Update i18n.config.js

--- i18n/i18n.config.js
+++ i18n/i18n.config.js
@@ -1,39 +1,38 @@
 
-import { createI18n } from 'vue-i18n'
-
+ 
 import english from './en_us.json'
 import chinese from './zh.json'
 import japanese from './ja.json'
 import korean from './ko.json'
-// export default defineI18nConfig(() => ({ 
-//     legacy: false,
-//     locale: 'en_us', 
-//     fallbackLocale: 'en_us', // Set fallback locale if a key is missing in another language
-//     warnHtmlInMessage: 'off', // disable of the Detected HTML in message
-//     messages: {
-//         'en_us': english,
-//         'zh': chinese,
-//         'ja': japanese,
-//         'ko': korean
-//     }
-// }))
+export default defineI18nConfig(() => ({ 
+    legacy: false,
+    locale: 'en_us', 
+    fallbackLocale: 'en_us', // Set fallback locale if a key is missing in another language
+    warnHtmlInMessage: 'off', // disable of the Detected HTML in message
+    messages: {
+        'en_us': english,
+        'zh': chinese,
+        'ja': japanese,
+        'ko': korean
+    }
+}))
 
 
-export default defineNuxtPlugin((nuxtApp) => {
-    const i18n = createI18n({
-        legacy: false,
-        globalInjection: true,
-        locale: 'en_us',
-        fallbackLocale: 'en_us',
-        messages: {
-            'en_us': english,
-            'zh': chinese,
-            'ja': japanese,
-            'ko': korean
-        }
-    })
-    // nuxtApp.vueApp.use(i18n)
-    nuxtApp.vueApp.use(i18n)
-})
+// export default defineNuxtPlugin((nuxtApp) => {
+//     const i18n = createI18n({
+//         legacy: false,
+//         globalInjection: true,
+//         locale: 'en_us',
+//         fallbackLocale: 'en_us',
+//         messages: {
+//             'en_us': english,
+//             'zh': chinese,
+//             'ja': japanese,
+//             'ko': korean
+//         }
+//     })
+//     // nuxtApp.vueApp.use(i18n)
+//     nuxtApp.vueApp.use(i18n)
+// })
 
  
